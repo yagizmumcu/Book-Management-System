99b97d7e6b713cd5d223952d3194440c006b4fd3
yagiz <ymumcu00@gmail.com>
2020-11-23 22:32:05 +0300

    First Commit- Pushed all the files

---
+++ BookMark/src/main/java/net/codejava/ProductManager/PassswordGenerator.java
@@ -0,0 +1,16 @@
+package net.codejava.ProductManager;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PassswordGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "asd123";
+		String encodedPassword = encoder.encode(rawPassword);
+		System.out.println(encodedPassword);
+
+	}
+
+}
---
+++ BookMark/src/main/java/net/codejava/ProductManager/UserRepository.java
@@ -0,0 +1,11 @@
+package net.codejava.ProductManager;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+	public User getUserByUserName(@Param("username") String username);
+}
